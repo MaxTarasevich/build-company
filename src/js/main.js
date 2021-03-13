e8332831c247390b6117b57e5712084d6da08221
Max <maxtarasevich@gmail.com>
2021-03-13 14:17:14 +0300

    dreams

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,23 @@
 
+window.onload = function () {
+
+// Tabs
+const tabsButtons = document.querySelectorAll(`.services__tabs-button`)
+const tabs = document.querySelectorAll(`.services__tabs-item`)
+
+tabs[0].classList.add(`active`)
+
+tabsButtons.forEach((button, index) => {
+    button.addEventListener(`click`, () =>{
+        tabChage(index)
+    })
+})
+
+function tabChage (number){
+    tabs.forEach((tab) => {
+        tab.classList.remove(`active`)
+    })
+    tabs[number].classList.add(`active`)
+}
+
+}
