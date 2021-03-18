d7720e67b2adfe7ab095d65830bd0545a9006a23
Max <maxtarasevich@gmail.com>
2021-03-19 01:40:25 +0300

    responsible tabe

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,45 @@
 
 window.onload = function () {
 
-// Tabs
-const tabsButtons = document.querySelectorAll(`.services__tabs-button`)
-const tabs = document.querySelectorAll(`.services__tabs-item`)
+tabsInitial(`.services__tabs-button`, `.services__tabs-item`)
 
-tabs[0].classList.add(`active`)
+tabsInitial(`.price__tabs-button`, `.price__tabs-table`)
 
-tabsButtons.forEach((button, index) => {
-    button.addEventListener(`click`, () =>{
-        tabChage(index)
-    })
-})
+clickTableButton ()
 
-function tabChage (number){
-    tabs.forEach((tab) => {
-        tab.classList.remove(`active`)
-    })
-    tabs[number].classList.add(`active`)
+
+function tabsInitial (buttonsClass, itemClass) {
+    const tabsButtons = document.querySelectorAll(buttonsClass)
+    const tabs = document.querySelectorAll(itemClass)
+
+        tabs[0].classList.add(`active`)
+
+        tabsButtons.forEach((button, index) => {
+            button.addEventListener(`click`, () =>{
+            tabChage(index)
+            })
+        })  
+        
+        function tabChage (number){
+            tabs.forEach((tab) => {
+                tab.classList.remove(`active`)
+            })
+            tabs[number].classList.add(`active`)
+        }
 }
 
-// Circle
-const circleItem = document.querySelectorAll(`.spinner__item`)
 
+function clickTableButton () {
+    const tableButtons = document.querySelectorAll(`.price__tabs-button`)
+    tableButtons[0].classList.add(`clicked`)
+    tableButtons.forEach((tableButton) => {
+        tableButton.addEventListener(`click`, (e) => {
+            tableButtons.forEach((tableButton) => {
+                tableButton.classList.remove(`clicked`)
+            })
+            e.target.classList.add(`clicked`)
+        })
+    })
 
-
+ }
 }
